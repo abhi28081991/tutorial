7850b718fca93f0e200e15d7328dd96d3a6c84cc
Abhijeet Kumar Ghosh <Abhijeet Kumar Ghosh@LAPTOP-VLI30U3S>
2018-10-01 21:59:19 +0300

    MASTER TICKET COMPLETE

--- masterticket.py
+++ masterticket.py
@@ -1,6 +1,5 @@
 Ticket_price = 10
 no_of_tickets= 100
-#output name of the person using the name variable 
 def username(Name):
     return Name
 #output how many tickets are remaining using the ticket_remaining variable 
@@ -11,18 +10,21 @@ def total_price(no_of_ticket_purchased):
 while no_of_tickets>=1:
     Name = input("please enter youre name ")
     user = username(Name)
+    try:
+        how_many_ticket = int(input("Please enter how many ticket you want to purchase "))
+        total = total_price(how_many_ticket)
+        no_of_tickets = remaining(how_many_ticket)
+        print("total ticket remaing {}".format(no_of_tickets))
+        print("Hello my dear {} youre purchase is successful final amount to pay is {} and total no of tickets are {}".format(user,total,how_many_ticket))
+    except ValueError:
+        print("please enter a number between 1 to {}".format(no_of_tickets))
 
-    how_many_ticket = int(input("Please enter how many ticket you want to purchase "))
-    total = total_price(how_many_ticket)
-    no_of_tickets = remaining(how_many_ticket)
-    print("total ticket remaing {}".format(no_of_tickets))
-    print("Hello my dear {} youre purchase is successful final amount to pay is {} and total no of tickets are {}".format(user,total,how_many_ticket))
+    
+    #remaining tickets
+    
     #prompt user you want to continue
-
     response = input("would you like to continue (Y/N) ")
     if response.upper() == 'Y':
-        
-        #remaining tickets
         print("total ticket remaing {}".format(no_of_tickets))
         
     else:
